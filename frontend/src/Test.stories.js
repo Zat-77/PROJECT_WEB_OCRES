b15457122e4579a13d5ffced6071d57dc7cfd980
Kruskal <vic.delerme@gmail.com>
2020-11-29 14:03:53 +0100

    Essaie d'ajout de story

---
+++ frontend/src/Test.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import progressBar from './../progressBar'
+
+export default {
+  title: 'ProgressBar',
+};
+
+export const ProgressBar = () => <ProgressBar/>
+
