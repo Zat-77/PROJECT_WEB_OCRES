5e6dc6483e7a78640d690abacbb02848398ef9da
Kruskal <vic.delerme@gmail.com>
2020-11-29 15:05:15 +0100

    Fin du Storyboook

---
+++ frontend/src/stories/Graphique.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import SimpleLineGraphe from './../simpleLineGraphe'
+
+export default {
+  title: 'Graphique',
+};
+
+export const simpleLineGraphe = () => <SimpleLineGraphe/>;
--- frontend/src/stories/Test.stories.js
+++ frontend/src/stories/Test.stories.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import progressBar from './../progressBar'
+import ProgressBar from './../progressBar'
 
 export default {
   title: 'ProgressBar',
 };
 
-export const ProgressBar = () => <ProgressBar/>
-
+export const progressBar = () => <ProgressBar/>;
